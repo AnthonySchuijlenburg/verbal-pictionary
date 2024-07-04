17e1e5f692fd56047e4c2deae12b8777460fd6bc
AnthonySchuijlenburg <a.schuijlenburg@gmail.com>
2024-07-04 12:06:47 +0200

    feat: setup application to be prepared for nuxt v4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   compatibilityDate: "2024-07-04",
+  future: {
+    compatibilityVersion: 4,
+  },
 });
