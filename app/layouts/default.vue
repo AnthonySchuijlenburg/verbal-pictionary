1ba2a4254f86981b974032b297281ba6c3a850d9
Anthony Schuijlenburg <a.schuijlenburg@gmail.com>
2024-07-10 20:48:39 +0200

    feat: add a max width to all pages

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <AppHeader />
-    <div class="m-4 mb-16">
+    <div
+      class="m-4 mt-8 mb-16 w-full max-w-3xl px-6 mx-auto md:max-w-4xl lg:px-0"
+    >
       <slot />
     </div>
   </div>
