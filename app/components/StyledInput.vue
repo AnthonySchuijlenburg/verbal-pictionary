c47428c3d9d4a59c289305e9e6a56c916222b396
Anthony Schuijlenburg <a.schuijlenburg@gmail.com>
2024-07-13 17:10:16 +0200

    fix: change font-size to 16px, to prevent zooming on mobile devices

--- app/components/StyledInput.vue
+++ app/components/StyledInput.vue
@@ -32,7 +32,7 @@ const modelValue = defineModel<string | number | null>();
       :maxlength="maxLength"
       :placeholder="$t(`${translationKey}.placeholder`)"
       v-bind="$attrs"
-      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+      class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
     />
   </div>
 </template>
