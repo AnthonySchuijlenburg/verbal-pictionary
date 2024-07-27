aa7a77e9d207ecae05dc0ce9d9eac8be9fb03acd
Anthony Schuijlenburg <a.schuijlenburg@gmail.com>
2024-07-27 17:32:29 +0200

    add link to homepage

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,9 +1,11 @@
 <template>
   <nav class="bg-white shadow-md sticky top-0 z-10">
-    <div class="w-full max-w-3xl px-6 mx-auto md:max-w-4xl lg:px-0 mx-auto">
+    <div class="w-full max-w-3xl px-6 md:max-w-4xl lg:px-0 mx-auto">
       <div class="flex space-x-4">
         <div class="flex justify-center items-center h-24">
-          <h1 class="font-medium text-2xl md:text-4xl">{{ $t("brand") }}</h1>
+          <NuxtLink to="/">
+            <h1 class="font-medium text-2xl md:text-4xl">{{ $t("brand") }}</h1>
+          </NuxtLink>
         </div>
       </div>
     </div>
