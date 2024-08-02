7e0aba33727eeadc4751a54b8df6d40c933390b9
AnthonySchuijlenburg <a.schuijlenburg@gmail.com>
2024-08-02 08:33:52 +0200

    feat: add debug page to check all translations

---
+++ app/pages/__debug/words.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const wordsStore = useWordsStore();
+</script>
+
+<template>
+  <div>
+    <h1 class="text-2xl font-medium">Words</h1>
+    <ul>
+      <li v-for="word in wordsStore.words.sort()" :key="word">
+        <span class="font-medium">{{ word }}</span> - {{ $t("words." + word) }}
+      </li>
+    </ul>
+  </div>
+</template>
