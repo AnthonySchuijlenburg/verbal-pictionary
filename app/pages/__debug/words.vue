fe32b0e6dc164c4121675c432d09da2bcc5f9fe7
Anthony Schuijlenburg <a.schuijlenburg@gmail.com>
2024-08-17 11:59:39 +0200

    fix: add the ability to check categories translations and remove some duplicates

--- app/pages/__debug/words.vue
+++ app/pages/__debug/words.vue
@@ -1,13 +1,34 @@
 <script setup lang="ts">
 const wordsStore = useWordsStore();
+const categoriesStore = useCategoriesStore();
+
+const categories = useCategories();
+
+onBeforeMount(() => {
+  // toggle all categories
+  categoriesStore.allEasy = false;
+  categoriesStore.allMedium = false;
+  categoriesStore.allHard = false;
+
+  categoriesStore.allEasy = true;
+  categoriesStore.allMedium = true;
+  categoriesStore.allHard = true;
+});
 </script>
 
 <template>
   <div>
-    <h1 class="text-2xl font-medium">Words</h1>
+    <h1 class="text-2xl">Categories</h1>
+    <ul>
+      <li v-for="category in Object.keys(categories)" :key="category">
+        <span class="font-bold">{{ category }}</span> -
+        {{ $t("categories." + category) }}
+      </li>
+    </ul>
+    <h1 class="text-2xl">Words</h1>
     <ul>
       <li v-for="word in wordsStore.words.sort()" :key="word">
-        <span class="font-medium">{{ word }}</span> - {{ $t("words." + word) }}
+        <span class="font-bold">{{ word }}</span> - {{ $t("words." + word) }}
       </li>
     </ul>
   </div>
